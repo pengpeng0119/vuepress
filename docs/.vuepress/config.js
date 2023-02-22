8ce1a85d57a68ca1aeb31bff4350c3e4df14a344
彭文豪 <2279265610@qq.com>
2023-02-22 23:58:53 +0800

    nav

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' },
+      { text: 'External', link: 'https://google.com' }
+    ]
+  }
+}
